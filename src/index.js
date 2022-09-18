a8b251f131553c704f30c1396e81e927a497765a
“tarek994” <ghali.tarek.123@hotmail.com>
2022-09-18 21:16:51 +0200

    change repo

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from "./App";
+
+import reportWebVitals from "./reportWebVitals 04-04-16-100";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
